a44a152414921920e655612743822c411a6952ee
Russell Gaspard <russg@fullsail.edu>
2013-10-03 20:11:19 -0400

    finally have both templates working

---
+++ App/platforms/android/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/russgaspard/Desktop/AVF_CODE/AVF_Project/App/platforms/android/bin/AndroidManifest.xml : \
+/Users/russgaspard/Desktop/AVF_CODE/AVF_Project/App/platforms/android/AndroidManifest.xml \
---
+++ App/platforms/android/bin/classes.dex.d
@@ -0,0 +1,10 @@
+/Users/russgaspard/Desktop/AVF_CODE/AVF_Project/App/platforms/android/bin/classes.dex : \
+/Users/russgaspard/Desktop/AVF_CODE/AVF_Project/App/platforms/android/bin/classes/com/fullsail/testApp/avfApp.class \
+/Users/russgaspard/Desktop/AVF_CODE/AVF_Project/App/platforms/android/bin/classes/com/fullsail/testApp/BuildConfig.class \
+/Users/russgaspard/Desktop/AVF_CODE/AVF_Project/App/platforms/android/bin/classes/com/fullsail/testApp/R$attr.class \
+/Users/russgaspard/Desktop/AVF_CODE/AVF_Project/App/platforms/android/bin/classes/com/fullsail/testApp/R$drawable.class \
+/Users/russgaspard/Desktop/AVF_CODE/AVF_Project/App/platforms/android/bin/classes/com/fullsail/testApp/R$layout.class \
+/Users/russgaspard/Desktop/AVF_CODE/AVF_Project/App/platforms/android/bin/classes/com/fullsail/testApp/R$string.class \
+/Users/russgaspard/Desktop/AVF_CODE/AVF_Project/App/platforms/android/bin/classes/com/fullsail/testApp/R$xml.class \
+/Users/russgaspard/Desktop/AVF_CODE/AVF_Project/App/platforms/android/bin/classes/com/fullsail/testApp/R.class \
+/Users/russgaspard/Desktop/AVF_CODE/AVF_Project/App/platforms/android/bin/dexedLibs/cordova-3.0.0-9cf6deb985b19fe3e51c9961e262f97f.jar \
