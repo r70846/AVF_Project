7f96a3e6a2b71bbc39f1e19884fea3127c2d504c
Russell Gaspard <russg@fullsail.edu>
2013-10-10 19:49:25 -0400

    project 2 crunch time

--- App/platforms/android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/russgaspard/Desktop/AVF_CODE/AVF_Project/App/platforms/android/bin/AndroidManifest.xml : \
-/Users/russgaspard/Desktop/AVF_CODE/AVF_Project/App/platforms/android/AndroidManifest.xml \
--- App/platforms/android/bin/avfApp-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Users/russgaspard/Desktop/AVF_CODE/AVF_Project/App/platforms/android/bin/avfApp-debug-unaligned.apk : \
-/Users/russgaspard/Desktop/AVF_CODE/AVF_Project/App/platforms/android/bin/avfApp.ap_ \
-/Users/russgaspard/Desktop/AVF_CODE/AVF_Project/App/platforms/android/bin/classes.dex \
-/Users/russgaspard/Desktop/AVF_CODE/AVF_Project/App/platforms/android/libs/cordova-3.0.0.jar \
