f3a2e67c68831a2965010ba856ebfd17c3d0b6e3
Russell Gaspard <russg@fullsail.edu>
2013-10-19 11:38:54 -0400

    finally got project2 droid working

--- bAVF/platforms/android/bin/AVFb-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Users/russgaspard/Desktop/AVF_CODE/AVF_Project/bAVF/platforms/android/bin/AVFb-debug-unaligned.apk : \
-/Users/russgaspard/Desktop/AVF_CODE/AVF_Project/bAVF/platforms/android/bin/AVFb.ap_ \
-/Users/russgaspard/Desktop/AVF_CODE/AVF_Project/bAVF/platforms/android/bin/classes.dex \
-/Users/russgaspard/Desktop/AVF_CODE/AVF_Project/bAVF/platforms/android/libs/cordova-3.0.0.jar \
--- bAVF/platforms/android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/russgaspard/Desktop/AVF_CODE/AVF_Project/bAVF/platforms/android/bin/AndroidManifest.xml : \
-/Users/russgaspard/Desktop/AVF_CODE/AVF_Project/bAVF/platforms/android/AndroidManifest.xml \
