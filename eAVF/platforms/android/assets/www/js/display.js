988e2698990a759e85ee4decf563245d63c74158
Russell Gaspard <russg@fullsail.edu>
2013-10-25 23:17:28 -0400

    final android working as well

--- eAVF/platforms/android/assets/www/js/display.js
+++ eAVF/platforms/android/assets/www/js/display.js
@@ -1,91 +1,138 @@
 // Russell Gaspard
-// Project 2
+// Project 4
 // AVF 1310
 // Mobile Development
 // Full Sail University
 
-/////// CODE FOR DISPLAY PAGE ////////////////////////////////
+/////// SET UP PAGE & EVENT BINDERS ////////////////////////////////
 
 document.addEventListener("deviceready", fnDeviceReady, false);
 
-	//$(function() { fnDeviceReady() });
-	
 function fnDeviceReady() {
         //alert("device ready");
-		$("#outputWords").html('<span id="wlabel"><h2>Words:</h2></span>');
-		$("#photoList").html('');
-		$('#kword').val('');
+    
+		$("#outputWords").html('<h2 id="wlabel">Words:</h2>');
+		$("#photoList").html('');				//Clear word display area & photo display areas
+		$('#kword').val('');                    //Clear keyword input field
         
         //Event binders
-        $('#goButton').on('click', fnGo);
-	};
+        $('#goButton').on('click', fnGo);		//Call function to process keyword
+        $('#home').on('click', fnHome);			//Call function to navigate home
+        
+        //Provide initial explanation/instruction
+        $("#message").html("<hr/><p>Enter a key word above to generate related words and photos.<p><hr/>");
+    
+};
 
-	function fnGo(eData){
-        //alert("Hello?");
-		eData.preventDefault();
-		$("#outputWords").html('<span id="wlabel"><h2>Words:</h2></span>');
-		$("#photoList").html('');
-		var tag = $('#kword').val();
-		fnInstagram(tag);
-		fnThesaurus(tag);
-	};
 
-	function fnThesaurus(word){
-		var address = "http://words.bighugelabs.com/api/2/cd242e86697b9eac71be92244bb679ad/" + word + "/json"
-		
-        $.ajax({
-            type: "GET",
-            url: address,
-            dataType: "jsonp",
-            jsonpCallback: 'fnDisplayWords'
-        });
-	};
+var fnHome = function(){
+	window.location.href = "index.html";
+};
 
-	function fnDisplayWords(info){
-		if(info.noun != undefined){
-			$.each(info.noun.syn, function(i, syn) {
-				console.log("noun");
-				//var strn = "<a href='#'><span> [" + syn + "] </span></a>";
-                var strn = "<span> " + syn + " :</span>";
-                   
-				$("#outputWords").append(strn);
-			}); 
-		};
-		if(info.verb != undefined){
-			$.each(info.verb.syn, function(i, syn) {
-				console.log("vrb");
-                var strn = "<span> " + syn + " :</span>";
-				$("#outputWords").append(strn);
-			}); 
-		};
-		if(info.adjective != undefined){
-			$.each(info.adjective.syn, function(i, syn) {
-				console.log("adj");
-                var strn = "<span> " + syn + " :</span>";
-				$("#outputWords").append(strn);
-			}); 
-		};	
-	}; 
-		
 
-	//Search Instagram for users tag word
-	function fnInstagram(word) {
-		var address = "https://api.instagram.com/v1/tags/" + word + 
+var fnConnection = function(){
+    
+    var sText = "";
+	var cType = navigator.connection.type;
+    var connection = {};
+    
+    connection[Connection.UNKNOWN]  = 'Unknown';
+    connection[Connection.ETHERNET] = 'Ethernet';
+    connection[Connection.WIFI]     = 'WiFi';
+    connection[Connection.CELL_2G]  = '2G Cell';
+    connection[Connection.CELL_3G]  = '3G Cell';
+    connection[Connection.CELL_4G]  = '4G Cell';
+    connection[Connection.CELL]     = 'Generic Cell';
+    connection[Connection.NONE]     = 'No Network';
+    
+    var sConnection = connection[cType];
+    
+    if(sConnection === 'No Network'){
+        sText = "<hr/><p>Service unavailable. No network connection at this time.</p><hr/>";
+    }else{
+        sText = "<hr/><p>Loading through"+ sConnection + " connection...</p><hr/>";
+        
+    }
+    
+    $("#message").html(sText);
+};
+
+
+var fnGo = function (eData){
+    //alert("Hello?");
+    eData.preventDefault();                     //Prevent page reload from button push
+    
+	var tag = $('#kword').val();				//Get keyword
+    if(tag.length > 0){
+    
+        $("#message").html('');                     //Clear Connection Message
+        $("#outputWords").html('<h2 id="wlabel">Words:</h2>');
+        $("#photoList").html('');					//Clear word display area & photo display areas
+        
+        fnConnection();							    //Call Show Connection type while loading
+        fnInstagram(tag);							//Call Instagram API function
+        fnThesaurus(tag);							//Call Thesaurus API function
+    };
+};
+
+/////// JSONP REMOTE DATA API 1 : INSTAGRAM /////////////////////////
+
+	//Search Instagram for users tag word: Setup callback
+var fnInstagram = function (word) {
+	var address = "https://api.instagram.com/v1/tags/" + word + 
 			"/media/recent?callback=?&amp;client_id=d51f6ee392ba41dfa0c28e580e9fa5da&amp;min_id=10";
-		$.getJSON(address, fnDisplayPics);
-	};
+	$.getJSON(address, fnDisplayPics);
+};
 
-
-	var fnDisplayPics = function (info){
-		//Clear display space of previous searches
-		
-		//Place each Photo and label into the list
-		$.each(info.data, function(i, pic) {
+										//Retrieve and append each photo into the list on our page
+var fnDisplayPics = function (info){	
+	$.each(info.data, function(i, pic) {
 		var photo = "<div class='imgWrap'><img src='" + pic.images.standard_resolution.url + "' /></div>";
-			$("#photoList").append(photo);
+		$("#photoList").append(photo);
+	});
+    $("#message").html('');            //Clear Connection Message
+};
+	
+/////// JSONP REMOTE DATA API 2 : THESAURUS /////////////////////////
+
+var fnThesaurus = function (word){						//Insert keyword in URL & fire AJAX call
+	var address = "http://words.bighugelabs.com/api/2/cd242e86697b9eac71be92244bb679ad/" + word + "/json"
+    $.ajax({
+        type: "GET",
+        url: address,
+        dataType: "jsonp",
+        jsonpCallback: 'fnDisplayWords'
+    });
+};
+
+var fnDisplayWords = function (info){
+	//We cannot know if return data has "noun", "verb" and/or "adjective" groupings..
+	//If statements search for each time and prevent throwing error id its not there..
+	
+	if(info.noun != undefined){	
+		$.each(info.noun.syn, function(i, syn) {
+			console.log("noun");
+			var strn = "<span> " + syn + " :</span>";   
+			$("#outputWords").append(strn);
 		}); 
 	};
-	
+	if(info.verb != undefined){
+		$.each(info.verb.syn, function(i, syn) {
+			console.log("vrb");
+            var strn = "<span> " + syn + " :</span>";
+			$("#outputWords").append(strn);
+		}); 
+	};
+	if(info.adjective != undefined){
+		$.each(info.adjective.syn, function(i, syn) {
+			console.log("adj");
+            var strn = "<span> " + syn + " :</span>";
+			$("#outputWords").append(strn);
+		}); 
+	};	
+}; 
+		
+
 	
 
 	
--- eAVF/platforms/android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/russgaspard/Desktop/AVF_CODE/AVF_Project/eAVF/platforms/android/bin/AndroidManifest.xml : \
-/Users/russgaspard/Desktop/AVF_CODE/AVF_Project/eAVF/platforms/android/AndroidManifest.xml \
